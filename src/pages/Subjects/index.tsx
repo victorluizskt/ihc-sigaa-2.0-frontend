a6d2da0909a427e9b2c61fc29aa7c96be1cc5b88
victor luiz <victorluiz@gmail.com>
2023-06-11 13:47:35 -0300

    add simple table

--- src/pages/Subjects/index.tsx
+++ src/pages/Subjects/index.tsx
@@ -3,7 +3,23 @@ import Header from "../../components/Header";
 const Subjects = () => (
     <>
         <Header />
-        <div>Subjects</div>
+        <table style={{border: "1"}}>
+        <tr>
+            <td>Nome</td>
+            <td>Idade</td>
+            <td>Profissão</td>
+        </tr>
+        <tr>
+            <td>Ted</td>
+            <td>22</td>
+            <td>Estudante</td>
+        </tr>
+        <tr>
+            <td>Ralf</td>
+            <td>26</td>
+            <td>Designer</td>
+        </tr>
+    </table>
     </>
 );
 
