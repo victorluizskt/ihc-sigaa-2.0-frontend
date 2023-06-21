9efdfe028ccd31f62965fb864db73926b9b6b423
victor luiz <victorluizcefet@gmail.com>
2023-06-21 20:53:14 -0300

    Adicionando estilo

---
+++ src/components/Sidebar/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const SidebarWrapper = styled.div`
+    * {
+        background: #FFF !important;
+    }
+    color: #000;
+    height: 100vh;
+    width: 50px;
+    transition: all 0.5s;
+    background: #FFF !important;
+`;
