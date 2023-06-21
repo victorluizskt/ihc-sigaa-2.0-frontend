536d851b326cc624e9ef9508c9e66407b3293eb6
victor luiz <victorluizcefet@gmail.com>
2023-06-21 20:53:33 -0300

    Adicionando estilo

---
+++ src/pages/Home/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+    background-color: #FFF;
+    width: 100%;
+    border-radius: 22px;
+    height: 148px;
+    * {
+        background-color: #FFF;
+    }
+`;
+
