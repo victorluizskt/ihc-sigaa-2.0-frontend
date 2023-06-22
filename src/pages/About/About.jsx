9734002150640f81a19d03f8db1d608fc91f7945
victor luiz <victorluizcefet@gmail.com>
2023-06-22 20:55:34 -0300

    Adicionando about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -75,9 +75,27 @@ const About = () => {
             { ListDegress.map(disciplina => (
                 <Card height="100px" marginTop="30px">
                     <div style={{ display: 'grid', gridTemplateColumns: '33% 33% 33%'}}>
-                        <div>PARTE 1</div>
-                        <div>PARTE 2</div>
-                        <div>PARTE 3</div>
+                        <div>
+                            <span>{disciplina.nomeDiciplina}</span>
+                            <span>{disciplina.icon}</span>
+                            <span>{disciplina.horario}</span>
+                            <span>{disciplina.horas}</span>
+                            <span>{disciplina.seeMore}</span>
+                        </div>
+                        <div>
+                            <span>{disciplina.nota}</span>
+                            <span>{disciplina.iconNota}</span>
+                            <span>{disciplina.pontoDistribuido}</span>
+                            <span>{disciplina.pontuacaoAtual}</span>
+                            <span>{disciplina.seeMoreNotas}</span>
+                        </div>
+                        <div>
+                            <span>{disciplina.frequencia}</span>
+                            <span>{disciplina.iconFrequencia}</span>
+                            <span>{disciplina.frequenciaRegistrada}</span>
+                            <span>{disciplina.frequenciaAtual}</span>
+                            <span>{disciplina.seeMoreFrequencia}</span>
+                        </div>
                     </div>
                 </Card>
             ))}
