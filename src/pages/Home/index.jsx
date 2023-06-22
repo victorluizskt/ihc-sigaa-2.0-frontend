95e6da500eccc5e004b2a75baa3e7b4142e07c32
victor luiz <victorluizcefet@gmail.com>
2023-06-22 18:48:14 -0300

    add second card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
 import Navbar from "../../components/Navbar";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import { Card } from "./styled";
+import News from '../../assets/news.png';
+import { Card, Line } from "./styled";
+
+const ListNews = [
+    { icon: News, nomeDisciplina: 'IHC', descricao: 'Introdução à Avaliação de IHC e Design da Solução (25/04/2023 - 25/04/2023)'},
+    { icon: News, nomeDisciplina: 'IHC', descricao: 'Qualidade em IHC e Abordagens Teóricas (20/04/2023 - 20/04/2023)'},
+    { icon: News, nomeDisciplina: 'IHC', descricao: 'Introdução à Avaliação de IHC e Design da Solução (25/04/2023 - 25/04/2023)'}
+
+]
 
 const Home = () => (
     <>
         <Sidebar>
             <Navbar />
-            <Card>
+            <Card height="20%">
                 <span style={{ 
                     fontSize: '24px', 
                     fontWeight: '700', 
@@ -40,6 +48,30 @@ const Home = () => (
                     width: '60%',
                     margin: '0 auto'
                 }}/>
+            </Card>
+            <Card height="50%" marginTop="40px">
+                <span style={{ 
+                    fontSize: '24px', 
+                    fontWeight: '700', 
+                    textAlign: 'center', 
+                    display: 'flex', 
+                    paddingTop: '26px',
+                    borderRadius: '22px',
+                    marginLeft: '30px',
+                    marginBottom: '10px',
+                }}>
+                    Últimas notícias cadastradas
+                </span>
+                { ListNews.map(item => (
+                    <div style={{ display: 'grid', gridTemplateRows: '18px 10px', marginTop: '5px' }}>
+                        <div>
+                        <img src={item.icon} alt="News" style={{ marginLeft: '30px', marginBottom: '-10px'}}/>
+                        <span style={{ marginLeft: '5px' ,fontSize: '15px'}}>Disciplina: {item.nomeDisciplina}</span>
+                        </div>
+                        <span style={{ marginLeft: '59px', fontSize: '10px'}}>Novo topico cadastrado: {item.descricao}</span>
+                        <Line />
+                    </div>
+                ))}
             </Card>
         </Sidebar>
     </>
