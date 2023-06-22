88ba981595e63cd023b6022f65ab0ea1f8120c50
victor luiz <victorluizcefet@gmail.com>
2023-06-22 18:50:26 -0300

    remove unecessary tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,70 +13,68 @@ const ListNews = [
 ]
 
 const Home = () => (
-    <>
-        <Sidebar>
-            <Navbar />
-            <Card height="20%">
-                <span style={{ 
-                    fontSize: '24px', 
-                    fontWeight: '700', 
+    <Sidebar>
+        <Navbar />
+        <Card height="20%">
+            <span style={{ 
+                fontSize: '24px', 
+                fontWeight: '700', 
+                textAlign: 'center', 
+                display: 'flex', 
+                justifyContent: 'center',
+                paddingTop: '26px',
+                borderRadius: '22px'
+            }}>
+                Procure por notícias
+            </span>
+            <span
+                style={{ 
+                    fontSize: '12px', 
                     textAlign: 'center', 
                     display: 'flex', 
                     justifyContent: 'center',
-                    paddingTop: '26px',
-                    borderRadius: '22px'
-                }}>
-                    Procure por notícias
-                </span>
-                <span
-                    style={{ 
-                        fontSize: '12px', 
-                        textAlign: 'center', 
-                        display: 'flex', 
-                        justifyContent: 'center',
-                        paddingTop: '5px',
-                        borderRadius: '22px',
-                        marginBottom: '10px'
-                    }}>
-                    Você poderá realizar buscas sobre artigos e notícias cadastradas pelos professores em suas disciplinas
-                </span>
-                <input 
-                    style={{
-                    textAlign: 'center', 
-                    display: 'flex', 
-                    height: '25px',
-                    marginTop: '10px',
-                    borderRadius: '8px',
-                    width: '60%',
-                    margin: '0 auto'
-                }}/>
-            </Card>
-            <Card height="50%" marginTop="40px">
-                <span style={{ 
-                    fontSize: '24px', 
-                    fontWeight: '700', 
-                    textAlign: 'center', 
-                    display: 'flex', 
-                    paddingTop: '26px',
+                    paddingTop: '5px',
                     borderRadius: '22px',
-                    marginLeft: '30px',
-                    marginBottom: '10px',
+                    marginBottom: '10px'
                 }}>
-                    Últimas notícias cadastradas
-                </span>
-                { ListNews.map(item => (
-                    <div style={{ display: 'grid', gridTemplateRows: '18px 10px', marginTop: '5px' }}>
-                        <div>
-                        <img src={item.icon} alt="News" style={{ marginLeft: '30px', marginBottom: '-10px'}}/>
-                        <span style={{ marginLeft: '5px' ,fontSize: '15px'}}>Disciplina: {item.nomeDisciplina}</span>
-                        </div>
-                        <span style={{ marginLeft: '59px', fontSize: '10px'}}>Novo topico cadastrado: {item.descricao}</span>
-                        <Line />
+                Você poderá realizar buscas sobre artigos e notícias cadastradas pelos professores em suas disciplinas
+            </span>
+            <input 
+                style={{
+                textAlign: 'center', 
+                display: 'flex', 
+                height: '25px',
+                marginTop: '10px',
+                borderRadius: '8px',
+                width: '60%',
+                margin: '0 auto'
+            }}/>
+        </Card>
+        <Card height="50%" marginTop="40px">
+            <span style={{ 
+                fontSize: '24px', 
+                fontWeight: '700', 
+                textAlign: 'center', 
+                display: 'flex', 
+                paddingTop: '26px',
+                borderRadius: '22px',
+                marginLeft: '30px',
+                marginBottom: '10px',
+            }}>
+                Últimas notícias cadastradas
+            </span>
+            { ListNews.map(item => (
+                <div style={{ display: 'grid', gridTemplateRows: '18px 10px', marginTop: '5px' }}>
+                    <div>
+                    <img src={item.icon} alt="News" style={{ marginLeft: '30px', marginBottom: '-10px'}}/>
+                    <span style={{ marginLeft: '5px' ,fontSize: '15px'}}>Disciplina: {item.nomeDisciplina}</span>
                     </div>
-                ))}
-            </Card>
-        </Sidebar>
-    </>
+                    <span style={{ marginLeft: '59px', fontSize: '10px'}}>Novo topico cadastrado: {item.descricao}</span>
+                    <Line />
+                </div>
+            ))}
+        </Card>
+    </Sidebar>
 )
 
 export default Home;
